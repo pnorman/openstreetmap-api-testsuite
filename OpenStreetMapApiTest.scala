d9efcde0d61faeb06c51914f9551768b7020f43c
Paul Norman <penorman@mac.com>
2013-07-22 23:17:18 -0700

    Move /node/# diff scenarios into Node.scala

--- OpenStreetMapApiTest.scala
+++ OpenStreetMapApiTest.scala
@@ -27,8 +27,8 @@ class ApiSimulation extends Simulation {
     .disableFollowRedirect
 
   setUp(
-    NodeScenario.nodeScn.inject(atOnce(1 users)),
-    NodeDiffScenario.nodeDiffScn.inject(nothingFor(1 seconds), atOnce(1 users)),
+    NodeScenarios.nodeScn.inject(atOnce(1 users)),
+    NodeScenarios.nodeDiffScn.inject(nothingFor(1 seconds), atOnce(1 users)),
     NodesScenario.nodesScn.inject(nothingFor(2 seconds), atOnce(1 users))
   )
   .protocols(httpProtocol)
